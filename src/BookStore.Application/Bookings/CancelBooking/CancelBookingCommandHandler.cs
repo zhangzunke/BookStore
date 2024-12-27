00b82bad1b3c1a8275be59ffb36b3ecde560b5b5
Mike Zhang <zhangzunke@126.com>
2024-12-27 17:16:00 +0800

    add cancel booking handler

---
+++ src/BookStore.Application/Bookings/CancelBooking/CancelBookingCommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using BookStore.Application.Abstractions.Clock;
+using BookStore.Application.Abstractions.Messaging;
+using BookStore.Domain.Abstractions;
+using BookStore.Domain.Bookings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.Bookings.CancelBooking
+{
+    internal sealed class CancelBookingCommandHandler : ICommandHandler<CancelBookingCommand>
+    {
+        private readonly IDateTimeProvider _dateTimeProvider;
+        private readonly IBookingRepository _bookingRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CancelBookingCommandHandler(
+            IDateTimeProvider dateTimeProvider,
+            IBookingRepository bookingRepository,
+            IUnitOfWork unitOfWork
+            )
+        {
+            _dateTimeProvider = dateTimeProvider;
+            _bookingRepository = bookingRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result> Handle(CancelBookingCommand request, CancellationToken cancellationToken)
+        {
+            var booking = await _bookingRepository.GetByIdAsync(request.BookingId, cancellationToken);
+
+            if (booking is null)
+            {
+                return Result.Failure(BookingErrors.NotFound);
+            }
+
+            var result = booking.Cancel(_dateTimeProvider.UtcNow);
+
+            if (result.IsFailure)
+            {
+                return result;
+            }
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Result.Success();
+        }
+    }
+}
